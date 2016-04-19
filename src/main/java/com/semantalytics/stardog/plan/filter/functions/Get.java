c2c09f0114fd469b0d2458493846bedb4470880e
Zachary Whitley <mail@zacharywhitley.com>
2016-04-19 12:41:07 -0400

    Minor refactoring

--- src/main/java/com/semantalytics/stardog/plan/filter/functions/Get.java
+++ src/main/java/com/semantalytics/stardog/plan/filter/functions/Get.java
@@ -11,48 +11,82 @@ import com.google.common.hash.Hashing;
 import com.google.common.io.Files;
 import org.apache.http.client.fluent.Request;
 import org.openrdf.model.Value;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
 
 public class Get extends AbstractFunction implements UserDefinedFunction {
 
+    private static final String SLASH = File.separator;
+    private static String downloadsDir;
+
+    static {
+        if(System.getenv("STARDOG_HOME") == null) {
+            downloadsDir = System.getProperty("user.home") + SLASH + "stardog-downloads";
+        } else {
+            downloadsDir = System.getenv("STARDOG_HOME") + SLASH + "downloads";
+        }
+    }
+
     public Get() {
         super(Range.closed(1, 1), "http://semantalytics.com/2016/04/ns/stardog/udf/httpclient/get");
     }
 
-    private Get(final Get httpClient) {
-        super(httpClient);
+    private File getDestinationFile(final String hash) {
+        return new File(downloadsDir + SLASH + hash.substring(0, 4) + SLASH + hash.substring(4,8) + SLASH + hash.substring(7));
+    }
+
+    private Get(final Get get) {
+        super(get);
     }
 
     @Override
     protected Value internalEvaluate(final Value... values) throws ExpressionEvaluationException {
+
         assertIRI(values[0]);
 
-        final File file;
+        final File tempFile;
+
         try {
-            file = File.createTempFile("stardog", ".tmp");
+            tempFile = File.createTempFile("stardog", ".tmp");
         } catch (IOException e) {
             throw new ExpressionEvaluationException("unable to create temp file", e);
         }
 
         try {
-            Request.Get(values[0].stringValue()).execute().saveContent(file);
+            Request.Get(values[0].stringValue()).execute().saveContent(tempFile);
         } catch (IOException e) {
-            throw new ExpressionEvaluationException("Unable to save temp file", e);
+            throw new ExpressionEvaluationException("Unable to write to temp file", e);
         }
-        final File destFile;
+
+        final String hash;
+
         try {
-            final String hash = Files.hash(file, Hashing.sha1()).toString();
-            final String stardogHome = System.getenv("STARDOG_HOME");
-            destFile = new File(stardogHome + "/downloads/" + hash.substring(0, 4) + "/" + hash.substring(4,8) + "/" + hash.substring(7));
-            Files.createParentDirs(destFile);
-            destFile.createNewFile();
-            Files.move(file, destFile);
+            hash = Files.hash(tempFile, Hashing.sha1()).toString();
         } catch (IOException e) {
             throw new ExpressionEvaluationException("Unable to calculate hash of file", e);
+        }
+
+        final File destFile = getDestinationFile(hash);
+
+        try {
+            Files.createParentDirs(getDestinationFile(hash));
+        } catch(IOException e) {
+            throw new ExpressionEvaluationException("Unable to create parent directories to final destination");
+        }
+
+        try {
+            destFile.createNewFile();
+        } catch(IOException e) {
+            throw new ExpressionEvaluationException("Unable to create destination file");
+        }
+
+        if (!destFile.exists()) {
+            try {
+                Files.move(tempFile, destFile);
+            } catch(IOException e) {
+                throw new ExpressionEvaluationException("Unable to copy temp final to destination");
+            }
         }
 
         return Values.iri(destFile.toURI().toString());
