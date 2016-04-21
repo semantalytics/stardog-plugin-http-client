df99213243c09b17c5e3b6bb4f384c50b08f95a9
Zachary Whitley <mail@zacharywhitley.com>
2016-04-21 13:11:34 -0400

    Add support for DELETE

--- src/main/java/com/semantalytics/stardog/plan/filter/functions/Delete.java
+++ src/main/java/com/semantalytics/stardog/plan/filter/functions/Delete.java
@@ -1,11 +1,19 @@
 package com.semantalytics.stardog.plan.filter.functions;
 
+import com.complexible.common.rdf.model.Values;
 import com.complexible.stardog.plan.filter.ExpressionEvaluationException;
 import com.complexible.stardog.plan.filter.ExpressionVisitor;
 import com.complexible.stardog.plan.filter.functions.AbstractFunction;
 import com.complexible.stardog.plan.filter.functions.Function;
 import com.complexible.stardog.plan.filter.functions.UserDefinedFunction;
+import com.google.common.hash.Hashing;
+import com.google.common.io.Files;
+import org.apache.http.client.fluent.Request;
+import org.apache.http.client.fluent.Response;
 import org.openrdf.model.Value;
+
+import java.io.File;
+import java.io.IOException;
 
 public class Delete extends AbstractFunction implements UserDefinedFunction {
 
@@ -19,7 +27,22 @@ public class Delete extends AbstractFunction implements UserDefinedFunction {
 
     @Override
     protected Value internalEvaluate(final Value... values) throws ExpressionEvaluationException {
-        return null;
+
+        assertIRI(values[0]);
+
+        Response response;
+
+        try {
+            response = Request.Delete(values[0].stringValue()).execute();
+        } catch (IOException e) {
+            throw new ExpressionEvaluationException("Unable to write to temp file", e);
+        }
+
+        try {
+            return Values.literal(response.returnResponse().getStatusLine().getStatusCode());
+        } catch (IOException e) {
+            throw new ExpressionEvaluationException("Unable to get response");
+        }
     }
 
     @Override
